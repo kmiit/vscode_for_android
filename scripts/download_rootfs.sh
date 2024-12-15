78f0a8cd516cdac0f71cf7593cad5fa448f742ca
Kmiit <2421768138@qq.com>
2024-12-15 18:32:00 +0000

    Change to archlinux

--- scripts/download_rootfs.sh
+++ scripts/download_rootfs.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 # Ubuntu rootfs, refer to https://github.com/termux/proot-distro/releases
-UBUNTU_DISTRO=noble
-UBUNTU_ARCH=aarch64
-UBUNTU_VERSION=v4.11.0
+DISTRO_TYPE=archlinux
+DISTRO_VARIANT=
+DISTRO_ARCH=aarch64
+DISTRO_VERSION=v4.6.0
+
+if [[ ${DISTRO_TYPE} != "" ]]; then
+    DISTRO_VARIANT=-${DISTRO_VARIANT}
+fi
+
+FILE_NAME=${DISTRO_TYPE}${DISTRO_VARIANT}-${DISTRO_ARCH}-pd-${DISTRO_VERSION}.tar.xz
 
 # bootstrap, refer to https://github.com/termux/termux-packages/releases/
 PROOT_DISTRO_URL=https://github.com/termux/proot-distro/archive/refs/heads/master.zip
-ROOTFS_URL=https://github.com/termux/proot-distro/releases/download/${UBUNTU_VERSION}/ubuntu-${UBUNTU_DISTRO}-${UBUNTU_ARCH}-pd-${UBUNTU_VERSION}.tar.xz
+ROOTFS_URL=https://github.com/termux/proot-distro/releases/download/${DISTRO_VERSION}/${FILE_NAME}
 PWD=$(dirname $(readlink -f "$0"))
 
 download_proot_distro() {
     wget $PROOT_DISTRO_URL -O ${PWD}/../assets/proot-distro.zip
 }
 download_rootfs() {
-    wget $ROOTFS_URL -O ${PWD}/../assets/ubuntu-${UBUNTU_DISTRO}-${UBUNTU_ARCH}-pd-${UBUNTU_VERSION}.tar.xz
+    wget $ROOTFS_URL -O ${PWD}/../assets/${FILE_NAME}
 }
 
 download_rootfs
